985ed9b14bba3edf230135d7803d45e8fafe6f9e
Khaled Hammoun <kh.hammoun@gmail.com>
2021-10-10 16:33:45 +0200

    FileContext.cs adapted to support User storage.

--- Assignment01-Adults-Blazor/Persistence/FileContext.cs
+++ Assignment01-Adults-Blazor/Persistence/FileContext.cs
@@ -8,17 +8,20 @@ namespace Assignment01_Adults_Blazor.Persistence
     public class FileContext
     {
         public IList<Adult> Adults { get; private set; }
+        public IList<User> Users { get; private set; }
 
         private readonly string adultsFile = "adults.json";
+        private readonly string usersFile = "users.json";
 
         public FileContext()
         {
             Adults = File.Exists(adultsFile) ? ReadData<Adult>(adultsFile) : new List<Adult>();
+            Users = File.Exists(usersFile) ? ReadData<User>(usersFile) : new List<User>();
         }
 
         private IList<T> ReadData<T>(string s)
         {
-            using (var jsonReader = File.OpenText(adultsFile))
+            using (var jsonReader = File.OpenText(s))
             {
                 return JsonSerializer.Deserialize<List<T>>(jsonReader.ReadToEnd());
             }
@@ -35,6 +38,17 @@ namespace Assignment01_Adults_Blazor.Persistence
             using (StreamWriter outputFile = new StreamWriter(adultsFile, false))
             {
                 outputFile.Write(jsonAdults);
+            }
+            
+            // storing users
+            string jsonUsers = JsonSerializer.Serialize(Users, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            using (StreamWriter outputFile = new StreamWriter(usersFile, false))
+            {
+                outputFile.Write(jsonUsers);
             }
         }
     }
