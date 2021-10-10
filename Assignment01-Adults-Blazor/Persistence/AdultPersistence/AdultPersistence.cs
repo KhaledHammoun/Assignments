c32ee952442d4d0a2e725a1ed6756bcdd43dca11
Khaled Hammoun <kh.hammoun@gmail.com>
2021-10-10 16:34:06 +0200

    Adult persistence storage adapter classes created.

---
+++ Assignment01-Adults-Blazor/Persistence/AdultPersistence/AdultPersistence.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assignment01_Adults_Blazor.Models;
+using Assignment01_Adults_Blazor.Persistence.AdultPersistance;
+
+namespace Assignment01_Adults_Blazor.Persistence.AdultPersistence
+{
+    public class AdultPersistence : IAdultPersistence
+    {
+        private FileContext fileContext;
+
+        public AdultPersistence(FileContext fileContext)
+        {
+            this.fileContext = fileContext;
+        }
+
+        public void AddAdult(Adult adult)
+        {
+            if (adult == null) throw new NullReferenceException("Adult must not be null");
+        
+            try
+            {
+                Adult exists = fileContext.Adults.FirstOrDefault(a => a.Equals(adult));
+                if (exists != null) throw new ArgumentException("Adult aready exists");
+                int newId = fileContext.Adults.Max(person => person.Id);
+                adult.Id = ++newId;
+                fileContext.Adults.Add(adult);
+                fileContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public List<Adult> GetAllAdults()
+        {
+            return fileContext.Adults.ToList();
+        }
+
+        public void RemoveAdult(int id)
+        {
+            Adult adult = fileContext.Adults.FirstOrDefault(a => a.Id.Equals(id));
+            fileContext.Adults.Remove(adult);
+            fileContext.SaveChanges();
+        }
+    }
+}
---
+++ Assignment01-Adults-Blazor/Persistence/AdultPersistence/IAdultPersistence.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Assignment01_Adults_Blazor.Models;
+
+namespace Assignment01_Adults_Blazor.Persistence.AdultPersistance
+{
+    public interface IAdultPersistence
+    {
+        void AddAdult(Adult adult);
+        List<Adult> GetAllAdults();
+        void RemoveAdult(int id);
+    }
+}
