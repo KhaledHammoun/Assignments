7dd135868baa85a4fd1f82203caa5a672864da1c
Khaled Hammoun <kh.hammoun@gmail.com>
2021-10-11 09:11:43 +0200

    No argument constructor added

--- Assignment01-Adults-Blazor/Models/Adult.cs
+++ Assignment01-Adults-Blazor/Models/Adult.cs
@@ -4,6 +4,10 @@ namespace Assignment01_Adults_Blazor.Models
     {
         public Job JobTitle { get; set; }
 
+        public Adult()
+        {
+            JobTitle = new Job();
+        }
         public Adult(int id, string firstName, string lastName, string hairColor, string eyeColor, int age, float weight, int height, string sex, Job jobTitle) : base(id, firstName, lastName, hairColor, eyeColor, age, weight, height, sex)
         {
             JobTitle = jobTitle;
