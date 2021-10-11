ffd62f53062752d5ef23c4f4fde045be81ccd76f
Khaled Hammoun <kh.hammoun@gmail.com>
2021-10-11 09:18:34 +0200

    No argument constructor added

--- Assignment01-Adults-Blazor/Models/User.cs
+++ Assignment01-Adults-Blazor/Models/User.cs
@@ -6,6 +6,10 @@
         public  string Password { set; get; }
         public string SecurityLevel { set; get; }
 
+        public User()
+        {
+            
+        }
         public User(int id, string firstName, string lastName, string hairColor, string eyeColor, int age, float weight, int height, string sex, Job jobTitle, string username, string password, string securityLevel) : base(id, firstName, lastName, hairColor, eyeColor, age, weight, height, sex, jobTitle)
         {
             Username = username;
