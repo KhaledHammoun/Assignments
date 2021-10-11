d9ce5bf37189a723740628ff81fec7f54189de22
Khaled Hammoun <kh.hammoun@gmail.com>
2021-10-11 09:15:24 +0200

    Added methods for getting and editing adult.

--- Assignment01-Adults-Blazor/Persistence/AdultPersistence/IAdultPersistence.cs
+++ Assignment01-Adults-Blazor/Persistence/AdultPersistence/IAdultPersistence.cs
@@ -8,5 +8,8 @@ namespace Assignment01_Adults_Blazor.Persistence.AdultPersistance
         void AddAdult(Adult adult);
         List<Adult> GetAllAdults();
         void RemoveAdult(int id);
+
+        Adult GetAdultById(int id);
+        void EditAdult(Adult adult);
     }
 }
