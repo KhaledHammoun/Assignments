6f763d6f269a737387165faea6566990a6ca5df3
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-01 11:45:03 +0100

    Api controllers for the two resources

---
+++ Assignment02/Server/Controllers/AdultsController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Model;
+using Server.Persistence.AdultPersistence;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdultsController : ControllerBase
+    {
+        private readonly IAdultPersistence adultPersistence;
+
+        public AdultsController(IAdultPersistence adultPersistence)
+        {
+            this.adultPersistence = adultPersistence;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<Adult>>> GetAllAdults([FromQuery] string firstName, [FromQuery] string jobTitle)
+        {
+            var adultsToReturn = new List<Adult>();
+            try
+            {
+                var adults = await adultPersistence.GetAllAdultsAsync();
+                adultsToReturn = adults.Where(a =>
+                    (string.IsNullOrEmpty(firstName) || a.FirstName.ToLower().Contains(firstName.ToLower())) &&
+                    (string.IsNullOrEmpty(jobTitle) || a.JobTitle.JobTitle.ToLower().Contains(jobTitle.ToLower()))).ToList();
+                
+                return Ok(adultsToReturn);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Adult>> GetAdultById(int id)
+        {
+            try
+            {
+                var adultToReturn = await adultPersistence.GetAdultByIdAsync(id);
+                return Ok(adultToReturn);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<Adult>> AddAdult([FromBody] Adult adult)
+        {
+            try
+            {
+                var adultToReturn = await adultPersistence.AddAdultAsync(adult);
+                return Created($"/{adultToReturn.Id}", adult);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult> DeleteAdult([FromRoute] int id)
+        {
+            try
+            {
+                await adultPersistence.RemoveAdultAsync(id);
+                Console.WriteLine(GetAllAdults(null, null));
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+        }
+        
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Adult>> EditAdult([FromBody] Adult adult)
+        {
+            try
+            {
+                var adultToReturn = await adultPersistence.EditAdultAsync(adult);
+                return Ok(adultToReturn);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ Assignment02/Server/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Model;
+using Server.Persistence.UserPersistence;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserPersistence userPersistence;
+
+        public UsersController(IUserPersistence userPersistence)
+        {
+            this.userPersistence = userPersistence;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<User>>> GetAllUsers()
+        {
+            try
+            {
+                var allUsersAsync = await userPersistence.GetAllUsersAsync();
+                return Ok(allUsersAsync);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<ActionResult<User>> CreateUser([FromBody] User user)
+        {
+            try
+            {
+                var userToReturn = await userPersistence.CreateUserAsync(user);
+                return Created($"/{userToReturn.Username}",userToReturn);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("validate")]
+        public async Task<ActionResult<User>> ValidateUser([FromBody] User user)
+        {
+            try
+            {
+                var validateUserAsync = await userPersistence.ValidateUserAsync(user);
+                return Ok(validateUserAsync);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
