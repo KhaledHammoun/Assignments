c227915a01e0c9c9d1d62e04240e4cd78f0f03de
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-01 11:44:15 +0100

    Model classes

---
+++ Assignment02/Shared/Models/Person.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Model {
+public class Person {
+    
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string HairColor { get; set; }
+    public string EyeColor { get; set; }
+    public int Age { get; set; }
+    public float Weight { get; set; }
+    public int Height { get; set; }
+    public string Sex { get; set; }
+
+    public Person()
+    {
+        
+    }
+    public Person(int id, string firstName, string lastName, string hairColor, string eyeColor, int age, float weight, int height, string sex)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        HairColor = hairColor;
+        EyeColor = eyeColor;
+        Age = age;
+        Weight = weight;
+        Height = height;
+        Sex = sex;
+    }
+}
+
+
+}
