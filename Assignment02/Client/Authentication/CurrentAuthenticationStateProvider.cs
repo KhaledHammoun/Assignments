5f72e43bdf83b6888117d2a8cd25db9cf6f9cc6e
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-01 11:43:48 +0100

    Edited so it fits the Restful communication

---
+++ Assignment02/Client/Authentication/CurrentAuthenticationStateProvider.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Client.Data.UserService;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using Model;
+
+namespace Client.Authentication
+{
+    public class CurrentAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly IJSRuntime jsRuntime;
+        private readonly IUserService userService;
+
+        private User cachedUser;
+
+        public CurrentAuthenticationStateProvider(IJSRuntime jsRuntime, IUserService userService)
+        {
+            this.jsRuntime = jsRuntime;
+            this.userService = userService;
+        }
+        
+        
+        
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity();
+            if (cachedUser == null)
+            {
+                string userAsJson = await jsRuntime.InvokeAsync<string>("sessionStorage.getItem", "currentUser");
+                if (!string.IsNullOrEmpty(userAsJson))
+                {
+                    User user = JsonSerializer.Deserialize<User>(userAsJson);
+                    ValidateUser(user.Username, user.Password);
+                }
+            }
+            else
+            {
+                identity = SetupClaimsForUser(cachedUser);
+            }
+
+            ClaimsPrincipal cachedClaimsPrincipal = new ClaimsPrincipal(identity);
+            return await Task.FromResult(new AuthenticationState(cachedClaimsPrincipal));
+        }
+
+        private ClaimsIdentity SetupClaimsForUser(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                // new Claim("Id", user.Id.ToString()),
+                // new Claim("FirstName", user.FirstName),
+                // new Claim("LastName", user.LastName),
+                // new Claim("HairColor", user.HairColor),
+                // new Claim("EyeColor", user.EyeColor),
+                // new Claim("Age", user.Age.ToString()),
+                // new Claim("Weight", user.Weight.ToString()),
+                // new Claim("Sex", user.Sex),
+                new Claim("Level", user.SecurityLevel.ToString())
+            };
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "apiauth_type");
+            return identity;
+        }
+
+        public async Task ValidateUser(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username)) throw new Exception("Enter username");
+            if (string.IsNullOrEmpty(password)) throw new Exception("Enter password");
+
+            var identity = new ClaimsIdentity();
+            try
+            {
+                var toVerify = new User {Username = username, Password = password};
+                var user = await userService.ValidateUser(toVerify);
+                identity = SetupClaimsForUser(user);
+                string userAsJson = JsonSerializer.Serialize(user);
+                await jsRuntime.InvokeVoidAsync("sessionStorage.setItem", "currentUser", userAsJson);
+                cachedUser = user;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity))));
+        }
+
+        public void Logout()
+        {
+            cachedUser = null;
+            var user = new ClaimsPrincipal(new ClaimsIdentity());
+            jsRuntime.InvokeVoidAsync("sessionStorage.setItem", "currentUser", user);
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+    }
+}
---
+++ Assignment02/Server/Persistence/AdultPersistence/AdultPersistence.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Model;
+
+namespace Server.Persistence.AdultPersistence
+{
+    public class AdultPersistence : IAdultPersistence
+    {
+        private FileContext fileContext;
+
+        public AdultPersistence(FileContext fileContext)
+        {
+            this.fileContext = fileContext;
+        }
+
+        public async Task<Adult> AddAdultAsync(Adult adult)
+        {
+            if (adult == null) throw new NullReferenceException("Adult must not be null");
+        
+            try
+            {
+                Adult exists = fileContext.Adults.FirstOrDefault(a => a.Equals(adult));
+                if (exists != null) throw new ArgumentException("Adult already exists");
+                int newId = fileContext.Adults.Max(person => person.Id);
+                adult.Id = ++newId;
+                fileContext.Adults.Add(adult);
+                fileContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return adult;
+        }
+
+        public async Task<IList<Adult>> GetAllAdultsAsync()
+        {
+            return fileContext.Adults.ToList();
+        }
+
+        public async Task RemoveAdultAsync(int id)
+        {
+            Adult adult = fileContext.Adults.FirstOrDefault(a => a.Id.Equals(id));
+            fileContext.Adults.Remove(adult);
+            fileContext.SaveChanges();
+        }
+
+        public async Task<Adult> GetAdultByIdAsync(int id)
+        {
+            Adult adultToReturn = fileContext.Adults.FirstOrDefault(a => a.Id.Equals(id));
+            if (adultToReturn == null) throw new Exception("Adult does not exist!");
+            return adultToReturn;
+        }
+
+        public async Task<Adult> EditAdultAsync(Adult adult)
+        {
+            fileContext.Adults[fileContext.Adults.ToList().FindIndex(index => index.Id.Equals(adult.Id))] = adult;
+            fileContext.SaveChanges();
+            return adult;
+        }
+    }
+}
